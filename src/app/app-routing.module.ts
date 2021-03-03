f560c1821e19a48e3f82079294878983bb5e30bc
Shnurok1342 <a13bepb@yandex.ru>
2021-03-03 12:15:02 +0300

    (lesson 3) feat: Routes, Paths and Outlets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,26 @@
 import {NgModule} from '@angular/core';
-import {Routes, RouterModule, PreloadAllModules, UrlSerializer} from '@angular/router';
-
+import {Routes, RouterModule} from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {AboutComponent} from './about/about.component';
 
 const routes: Routes = [
-
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  }
 ];
 
 @NgModule({
   imports: [
-
+    RouterModule.forRoot(routes)
   ],
   exports: [RouterModule],
   providers: [
 
   ]
 })
-export class AppRoutingModule {
-
-
-}
+export class AppRoutingModule {}
