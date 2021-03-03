41647e94ea00c359fe07f427f499876e254cda6e
Shnurok1342 <a13bepb@yandex.ru>
2021-03-03 19:51:03 +0300

    (lesson 21) feat: What is an Angular Router Preloading Strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
+import {Routes, RouterModule, NoPreloading} from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {AboutComponent} from './about/about.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
@@ -32,7 +32,11 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(
+      routes, {
+        preloadingStrategy: NoPreloading
+      }
+    )
   ],
   exports: [RouterModule],
   providers: [
