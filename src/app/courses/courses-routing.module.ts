ed1f8d43a40355497a196410224a65dd91d418d0
Shnurok1342 <a13bepb@yandex.ru>
2021-03-03 14:03:45 +0300

    (lesson 10-11) feat: Child Routes - Lessons List Component

--- src/app/courses/courses-routing.module.ts
+++ src/app/courses/courses-routing.module.ts
@@ -3,6 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {CourseComponent} from './course/course.component';
 import {CourseResolver} from './services/course.resolver';
+import {LessonDetailComponent} from './lesson/lesson-detail.component';
+import {LessonsListComponent} from './lessons-list/lessons-list.component';
+import {LessonsResolver} from './services/lessons.resolver';
 
 const routes: Routes = [
   {
@@ -12,6 +15,19 @@ const routes: Routes = [
   {
     path: ':courseUrl',
     component: CourseComponent,
+    children: [
+      {
+        path: '',
+        component: LessonsListComponent,
+        resolve: {
+          lessons: LessonsResolver
+        }
+      },
+      {
+        path: 'lessons/:lessonSeqNo',
+        component: LessonDetailComponent
+      }
+    ],
     resolve: {
       course: CourseResolver
     }
@@ -24,7 +40,8 @@ const routes: Routes = [
   ],
   exports: [RouterModule],
   providers: [
-    CourseResolver
+    CourseResolver,
+    LessonsResolver
   ]
 })
 export class CoursesRoutingModule {}
---
+++ src/app/courses/services/lessons.resolver.ts
@@ -0,0 +1,16 @@
+import {LessonSummary} from '../model/lesson-summary';
+import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/router';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {CoursesService} from './courses.service';
+
+@Injectable()
+export class LessonsResolver implements Resolve<LessonSummary[]> {
+  constructor(private coursesService: CoursesService) {}
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
+    Observable<LessonSummary[]> {
+    const courseUrl = route.paramMap.get('courseUrl');
+    return this.coursesService.loadAllCourseLessonsSummary(courseUrl);
+  }
+}
